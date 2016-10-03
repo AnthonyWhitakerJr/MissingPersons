91a01075c458b9bfbb2dd8e6e4941ea1724d1abb
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-10-03 16:02:28 -0400

    Create UI

--- MissingPersons/ViewController.swift
+++ MissingPersons/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var selectedImage: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func checkForMatchPressed(_ sender: AnyObject) {
+    }
+    
 }
 
